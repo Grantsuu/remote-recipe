5ab42853f392285b35337d7c97b605e6c0e28c27
Grant Sherman <tnarg122@gmail.com>
2024-01-06 06:16:35 +0000

    backend get recipes

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type Recipe struct {
+	ID     uint   `gorm:"primaryKey"`
+	Name   string `gorm:"not null"`
+	Author string `gorm:"not null"`
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	dsn := "host=localhost user=postgres password=your_password dbname=your_database port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	// Auto migrate the Recipe model
+	db.AutoMigrate(&Recipe{})
+
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	r.GET("/recipes", func(c *gin.Context) {
+		var recipes []Recipe
+		db.Find(&recipes)
+		c.JSON(200, recipes)
+	})
+
+	r.Run(":8080")
 }
