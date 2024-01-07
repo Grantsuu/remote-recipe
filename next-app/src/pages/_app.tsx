5e0b848b0d4d5b158abee0255ff2b20601112e6c
Grant Sherman <tnarg122@gmail.com>
2024-01-07 21:27:40 +0000

    bootstrap

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+        require('bootstrap/dist/js/bootstrap.bundle.min.js');
+      }, []);
+    return <Component {...pageProps} />
 }
--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -31,55 +31,27 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <title>Create Next app</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js</a> on Docker Compose!
+                    Project Remote-Recipes
                 </h1>
                 <p className={styles.description}>
-                    Get started by editing{' '}
-                    <code className={styles.code}>pages/index.tsx</code>
+                    A simple <i>Next.js</i> app to display recipes, hosted on a remote virtual machine.
                 </p>
+                <button type="button" className="btn btn-primary">Base class</button>
                 {recipes.map((recipe, index) => (
                     <div key={index}>
                         <h2>{recipe.Name}</h2>
                         <p>{recipe.Description}</p>
+                        <p>{recipe.Ingredients}</p>
+                        <p>{recipe.Directions}</p>
                     </div>
                 ))}
-                <div className={styles.grid}>
-                    <a href="https://nextjs.org/docs" className={styles.card}>
-                        <h3>Documentation &rarr;</h3>
-                        <p>Find in-depth information about Next.js features and API.</p>
-                    </a>
-
-                    <a href="https://nextjs.org/learn" className={styles.card}>
-                        <h3>Learn &rarr;</h3>
-                        <p>Learn about Next.js in an interactive course with quizzes!</p>
-                    </a>
-
-                    <a
-                        href="https://github.com/vercel/next.js/tree/master/examples"
-                        className={styles.card}
-                    >
-                        <h3>Examples &rarr;</h3>
-                        <p>Discover and deploy boilerplate example Next.js projects.</p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={styles.card}
-                    >
-                        <h3>Deploy &rarr;</h3>
-                        <p>
-                            Instantly deploy your Next.js site to a public URL with Vercel.
-                        </p>
-                    </a>
-                </div>
             </main>
 
             <footer className={styles.footer}>
