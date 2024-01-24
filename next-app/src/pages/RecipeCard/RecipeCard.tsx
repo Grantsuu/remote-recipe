3a5925bef0d4ab4f40312c36cabec66504bf8a3a
Grant Sherman <tnarg122@gmail.com>
2024-01-24 04:02:25 +0000

    delete with animation works

--- next-app/src/pages/RecipeCard/RecipeCard.tsx
+++ next-app/src/pages/RecipeCard/RecipeCard.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { faPenToSquare, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { AnimatePresence, motion } from "framer-motion"
 
 interface RecipeCardProps {
     host: string;
@@ -17,6 +18,8 @@ interface RecipeCardProps {
 }
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ host, recipe, recipeUpdates, setRecipeUpdates }) => {
+    const [isShown, setIsShown] = useState(true);
+
     const ingredientsArray: string[] = recipe.Ingredients.split(",");
     const directionsArray: string[] = recipe.Directions.split(/\d+\.\s/).filter(step => step.trim() !== "");
 
@@ -50,9 +53,9 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ host, recipe, recipeUpdates, se
     }
 
     const handleDeleteRecipe = async () => {
+        console.log("Deleting recipe with id: " + recipe.Id);
         await deleteRecipe(recipe.Id)
             .catch(err => console.error(err))
-        setRecipeUpdates(++recipeUpdates);
     }
 
     const DeleteRecipeModal = () => {
@@ -69,7 +72,7 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ host, recipe, recipeUpdates, se
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={handleDeleteRecipe}>Delete</button>
+                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Delete</button>
                         </div>
                     </div>
                 </div>
@@ -78,47 +81,56 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ host, recipe, recipeUpdates, se
     }
 
     return (
-        <>
-            <div className="container-fluid mb-4">
-                <div className="card bg-light">
-                    <div className="card-header d-flex justify-content-between align-items-center">
-                        <h4 className="my-0">{recipe.Name}</h4>
-                        <div className="ml-auto">
-                            <button type="button" className="btn btn-lg">
-                                <FontAwesomeIcon icon={faPenToSquare as IconProp} />
-                            </button>
-                            <button type="button" className="btn btn-lg" data-bs-toggle="modal" data-bs-target={"#deleteRecipeModal" + recipe.Id}>
-                                <FontAwesomeIcon icon={faXmark as IconProp} />
-                            </button>
+        <AnimatePresence>
+            {isShown && <motion.div
+                initial={{ opacity: 1}}
+                exit={{ opacity: 0}}
+                transition={{ duration: 0.25 }}
+                onAnimationComplete={()=>{
+                    handleDeleteRecipe()
+                }}
+            >
+                <div className="container-fluid mb-4" id={"recipe-card-" + recipe.Id}>
+                    <div className="card bg-light">
+                        <div className="card-header d-flex justify-content-between align-items-center">
+                            <h4 className="my-0">{recipe.Name}</h4>
+                            <div className="ml-auto">
+                                <button type="button" className="btn btn-lg" onClick={()=>{setIsShown(false)}}>
+                                    <FontAwesomeIcon icon={faPenToSquare as IconProp} />
+                                </button>
+                                <button type="button" className="btn btn-lg" data-bs-toggle="modal" data-bs-target={"#deleteRecipeModal" + recipe.Id}>
+                                    <FontAwesomeIcon icon={faXmark as IconProp} />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            <p className="card-text">
+                                {recipe.Description}.
+                            </p>
+                            <div className="mb-3">
+                                <h6>Ingredients:</h6>
+                                <ul className="list-group">
+                                    {ingredientsArray.map((ingredient, index) => (
+                                        <IngredientCheckbox key={index} ingredient={ingredient} index={index} />
+                                    ))}
+                                </ul>
+                            </div>
+                            <div>
+                                <h6>Directions:</h6>
+                                <ol className="list-group list-group-numbered">
+                                    {directionsArray.map((step, index) => (
+                                        <li key={index} className="list-group-item">
+                                            {step}
+                                        </li>
+                                    ))}
+                                </ol>
+                            </div>
                         </div>
                     </div>
-                    <div className="card-body">
-                        <p className="card-text">
-                            {recipe.Description}.
-                        </p>
-                        <div className="mb-3">
-                            <h6>Ingredients:</h6>
-                            <ul className="list-group">
-                                {ingredientsArray.map((ingredient, index) => (
-                                    <IngredientCheckbox key={index} ingredient={ingredient} index={index} />
-                                ))}
-                            </ul>
-                        </div>
-                        <div>
-                            <h6>Directions:</h6>
-                            <ol className="list-group list-group-numbered">
-                                {directionsArray.map((step, index) => (
-                                    <li key={index} className="list-group-item">
-                                        {step}
-                                    </li>
-                                ))}
-                            </ol>
-                        </div>
-                    </div>
+                    <DeleteRecipeModal />
                 </div>
-            </div>
-            <DeleteRecipeModal />
-        </>
+            </motion.div>}
+        </AnimatePresence>
     );
 };
 
